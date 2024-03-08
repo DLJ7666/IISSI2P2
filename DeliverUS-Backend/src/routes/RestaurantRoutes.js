6393cac4c290dc648d8f729fc181a4b98a4eafba
DLJ7666 <jarguezduran@gmail.com>
2024-03-08 08:59:57 +0100

    commit practica finalizada con exito (en casa)

--- DeliverUS-Backend/src/routes/RestaurantRoutes.js
+++ DeliverUS-Backend/src/routes/RestaurantRoutes.js
@@ -1,7 +1,17 @@
 import RestaurantController from '../controllers/RestaurantController.js'
 
 const loadFileRoutes = function (app) {
-  // TODO: Include routes for restaurant described in the lab session README
-
+  app.route('/restaurants') // the endpoint path
+    .get( // the http verb that we want to be available at the previous path
+      RestaurantController.index) // the function that will attend requests for that http verb and that path
+    .post( // we can chain more http verbs for the same endpoint
+      RestaurantController.create) // the function that will attend requests for that http verb and that path
+  app.route('/restaurants/:restaurantId')
+    .get(
+      RestaurantController.show)
+    .put(
+      RestaurantController.update)
+    .delete(
+      RestaurantController.destroy)
 }
 export default loadFileRoutes
